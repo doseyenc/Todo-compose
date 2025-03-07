218bbf858b50c0c18287992e5a4d388a1b9697dc
Çağrı Döşeyen <doseyenc@gmail.com>
2025-03-07 15:52:56 +0300

    module and repository added

---
+++ app/src/main/java/com/doseyenc/todo/data/repositories/ToDoRepository.kt
@@ -0,0 +1,35 @@
+package com.doseyenc.todo.data.repositories
+
+import com.doseyenc.todo.data.ToDoDao
+import com.doseyenc.todo.data.models.ToDoTask
+import javax.inject.Inject
+
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+
+    val getAllTasks = toDoDao.getAllTasks()
+
+    val sortByLowPriority = toDoDao.sortByLowPriority()
+
+    val sortByHighPriority = toDoDao.sortByHighPriority()
+
+    fun searchDatabase(searchQuery: String) = toDoDao.searchDatabase(searchQuery)
+
+    fun getSelectedTask(taskId: Int) = toDoDao.getSelectedTask(taskId)
+
+    suspend fun addTask(toDoTask: ToDoTask) {
+        toDoDao.addTask(toDoTask)
+    }
+
+    suspend fun updateTask(toDoTask: ToDoTask) {
+        toDoDao.updateTask(toDoTask)
+    }
+
+    suspend fun deleteTask(toDoTask: ToDoTask) {
+        toDoDao.deleteTask(toDoTask)
+    }
+
+    suspend fun deleteAllTasks() {
+        toDoDao.deleteAllTasks()
+    }
+
+}
