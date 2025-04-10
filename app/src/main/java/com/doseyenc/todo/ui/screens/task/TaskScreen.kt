a771767b5c43e31e21d7689ad0a96b8013e40fe4
Çağrı Döşeyen <doseyenc@gmail.com>
2025-04-10 23:05:49 +0300

    fields validation added

--- app/src/main/java/com/doseyenc/todo/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/doseyenc/todo/ui/screens/task/TaskScreen.kt
@@ -1,10 +1,13 @@
 package com.doseyenc.todo.ui.screens.task
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.doseyenc.todo.data.models.Priority
 import com.doseyenc.todo.data.models.ToDoTask
 import com.doseyenc.todo.ui.viewmodels.SharedViewModel
@@ -21,10 +24,22 @@ fun TaskScreen(
     val desc: String by sharedViewModel.description
     val priority: Priority by sharedViewModel.priority
 
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TaskAppBar(
-                navigateToListScreen = navigateToListScreen,
+                navigateToListScreen = { action ->
+                    if (action == Action.NO_ACTION) {
+                        navigateToListScreen(action)
+                    } else {
+                        if (sharedViewModel.validateFields()) {
+                            navigateToListScreen(action)
+                        } else {
+                            displayToast(context = context)
+                        }
+                    }
+                },
                 selectedTask = selectedTask
             )
         },
@@ -40,4 +55,12 @@ fun TaskScreen(
             )
         }
     )
+}
+
+fun displayToast(context: Context) {
+    Toast.makeText(
+        context,
+        "Fields Empty",
+        Toast.LENGTH_SHORT
+    ).show()
 }
